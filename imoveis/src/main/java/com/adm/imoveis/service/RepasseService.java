dedead9bf23dc51beb7f0c9bf76fe3eeae6afcec
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-04-15 20:58:49 -0300

    reafactor(RepasseService): conversao de dtos

--- imoveis/src/main/java/com/adm/imoveis/service/RepasseService.java
+++ imoveis/src/main/java/com/adm/imoveis/service/RepasseService.java
@@ -1,10 +1,10 @@
 package com.adm.imoveis.service;
 
-import java.time.YearMonth;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.adm.imoveis.dto.DtoUtils;
 import com.adm.imoveis.dto.repasse.RepasseCreationDto;
 import com.adm.imoveis.dto.repasse.RepasseDto;
 import com.adm.imoveis.entities.Contrato;
@@ -12,41 +12,29 @@ import com.adm.imoveis.entities.Repasse;
 import com.adm.imoveis.repositories.ContratoRepository;
 import com.adm.imoveis.repositories.RepasseRepository;
 import com.adm.imoveis.service.exception.ContratoNotFound;
-import com.adm.imoveis.utils.DateUtil;
 
 @Service
 public class RepasseService {
     private final RepasseRepository repasseRepository;
-    private final ContratoRepository contratoRepository;
+    private final ContratoRepository contratoRepository;  
 
     public RepasseService(RepasseRepository repasseRepository, ContratoRepository contratoRepository) {
         this.repasseRepository = repasseRepository;
         this.contratoRepository = contratoRepository;
     }
 
-    public RepasseDto convertModelToDto(Repasse repasse) {
-        return new RepasseDto(
-            repasse.getId(),
-            repasse.getDataRepasse(),
-            repasse.getValorRepasse(),
-            repasse.getContrato()
-        );
-    }
-
-    public List<RepasseDto> convertModelListToDto(List<Repasse> repasses) {
-        return repasses.stream().map((i) -> convertModelToDto(i)).toList();
-    }
 
    public RepasseDto create(RepasseCreationDto repasse) {
         Repasse newRepasse = new Repasse(repasse.valorRepasse(), repasse.dataRepasse());
         Contrato contrato = contratoRepository.findById(repasse.contratoId()).orElseThrow(ContratoNotFound::new);
         newRepasse.setContrato(contrato);
         Repasse createdRepasse = repasseRepository.save(newRepasse);
-        return convertModelToDto(createdRepasse);
+        return DtoUtils.repasseModelToDto(createdRepasse);
    }
 
    public List<RepasseDto> findAll() {
-    return convertModelListToDto(repasseRepository.findAll());
+    List<Repasse> repasseList = repasseRepository.findAll();
+    return DtoUtils.convertModelList(repasseList, DtoUtils::repasseModelToDto);
    }
 
     
