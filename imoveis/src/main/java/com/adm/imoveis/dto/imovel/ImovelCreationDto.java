17d31e39645e9eafdb310ec779886eea68366b2c
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-04-15 20:42:47 -0300

    reafactor(ImovelService): conversao de dtos

--- imoveis/src/main/java/com/adm/imoveis/dto/imovel/ImovelCreationDto.java
+++ imoveis/src/main/java/com/adm/imoveis/dto/imovel/ImovelCreationDto.java
@@ -1,5 +1,5 @@
 package com.adm.imoveis.dto.imovel;
 
-public record ImovelCreationDto(String apto, String status, int tamanho, String tipo, Long predioId) {
+public record ImovelCreationDto(String apto, String status, int tamanho, String tipo, Long predioId, Double valor) {
     
 }
--- imoveis/src/main/java/com/adm/imoveis/service/ImovelService.java
+++ imoveis/src/main/java/com/adm/imoveis/service/ImovelService.java
@@ -1,13 +1,12 @@
 package com.adm.imoveis.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.adm.imoveis.dto.DtoUtils;
 import com.adm.imoveis.dto.imovel.ImovelCreationDto;
 import com.adm.imoveis.dto.imovel.ImovelDto;
-import com.adm.imoveis.dto.predio.PredioDto;
 import com.adm.imoveis.entities.Imovel;
 import com.adm.imoveis.entities.Predio;
 import com.adm.imoveis.repositories.ImovelRepository;
@@ -32,41 +31,28 @@ public class ImovelService {
      * @return          the created Imovel
      */
     public ImovelDto create(ImovelCreationDto imovel) {
-        Imovel newImovel = new Imovel(imovel.tipo(), imovel.tamanho(), imovel.apto(), imovel.status());
+        Imovel newImovel = new Imovel(imovel.tipo(), imovel.tamanho(), imovel.apto(), imovel.status(), imovel.valor());
         Predio predio = predioRepository.findById(imovel.predioId()).orElseThrow(PredioNotFound::new);
         newImovel.setPredio(predio);
         Imovel createdImovel = imovelRepository.save(newImovel);
-        ImovelDto imovelResponse = convertImovelModeltoDto(createdImovel);
+        ImovelDto imovelResponse = DtoUtils.imovelModelToDto(createdImovel);
         return imovelResponse;
     }
 
-    public static ImovelDto convertImovelModeltoDto(Imovel imovel) {
-        return new ImovelDto(
-            imovel.getId(),
-            imovel.getTipo(),
-            imovel.getTamanho(),
-            imovel.getApto(),
-            imovel.getStatus(),
-            imovel.getPredio()
-        );
-    }
-
-    public static List<ImovelDto> convertImovelModelListtoDto(List<Imovel> imoveis) {
-        List<ImovelDto> response = imoveis.stream().map((i) -> convertImovelModeltoDto(i)).collect(Collectors.toList());
-        return response;
-    }
 
     public List<ImovelDto> getAll() {
-        return convertImovelModelListtoDto(imovelRepository.findAll());
+        List<Imovel> imovelList = imovelRepository.findAll();
+        return DtoUtils.convertModelList(imovelList, DtoUtils::imovelModelToDto);
     }
 
     public List<ImovelDto> getByPredioId(Long predioId) { 
         Predio predio = predioRepository.findById(predioId).orElseThrow(PredioNotFound::new);
-        List<Imovel> imoveis = imovelRepository.findByPredio(predio);
-        return convertImovelModelListtoDto(imoveis);
+        List<Imovel> imovelList = imovelRepository.findByPredio(predio);
+        return DtoUtils.convertModelList(imovelList, DtoUtils::imovelModelToDto);
     }
 
     public ImovelDto getById(Long id) {
-        return convertImovelModeltoDto(imovelRepository.findById(id).orElseThrow(ImovelNotFound::new));
+        Imovel imovel = imovelRepository.findById(id).orElseThrow(ImovelNotFound::new);
+        return DtoUtils.imovelModelToDto(imovel);
     }
 }
