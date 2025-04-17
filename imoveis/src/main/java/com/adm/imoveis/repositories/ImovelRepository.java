3938004a7efe3cea16739165e917b1438439fdf8
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-04-16 21:23:14 -0300

    feat(ImovelService): endpoints extras

--- imoveis/src/main/java/com/adm/imoveis/repositories/ImovelRepository.java
+++ imoveis/src/main/java/com/adm/imoveis/repositories/ImovelRepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.adm.imoveis.entities.Imovel;
 import com.adm.imoveis.entities.Predio;
+import com.adm.imoveis.entities.enums.ImovelStatus;
+import com.adm.imoveis.entities.enums.ImovelTipo;
+
+
 
 public interface ImovelRepository extends JpaRepository<Imovel, Long> {
     List<Imovel> findByPredio(Predio predio);
+    List<Imovel> findByStatus(ImovelStatus status);
+    List<Imovel> findByTipo(ImovelTipo tipo);    
 }
--- imoveis/src/main/java/com/adm/imoveis/service/ImovelService.java
+++ imoveis/src/main/java/com/adm/imoveis/service/ImovelService.java
@@ -9,6 +9,8 @@ import com.adm.imoveis.dto.imovel.ImovelCreationDto;
 import com.adm.imoveis.dto.imovel.ImovelDto;
 import com.adm.imoveis.entities.Imovel;
 import com.adm.imoveis.entities.Predio;
+import com.adm.imoveis.entities.enums.ImovelStatus;
+import com.adm.imoveis.entities.enums.ImovelTipo;
 import com.adm.imoveis.repositories.ImovelRepository;
 import com.adm.imoveis.repositories.PredioRepository;
 import com.adm.imoveis.service.exception.ImovelNotFound;
@@ -40,6 +42,7 @@ public class ImovelService {
     }
 
 
+
     public List<ImovelDto> getAll() {
         List<Imovel> imovelList = imovelRepository.findAll();
         return DtoUtils.convertModelList(imovelList, DtoUtils::imovelModelToDto);
@@ -54,5 +57,31 @@ public class ImovelService {
     public ImovelDto getById(Long id) {
         Imovel imovel = imovelRepository.findById(id).orElseThrow(ImovelNotFound::new);
         return DtoUtils.imovelModelToDto(imovel);
+    }
+
+    public ImovelDto update(Long id, ImovelCreationDto body) {
+        Imovel imovel = imovelRepository.findById(id).orElseThrow(ImovelNotFound::new);
+        imovel.setApto(body.apto());
+        imovel.setStatus(body.status());
+        imovel.setTamanho(body.tamanho());
+        imovel.setTipo(body.tipo());
+        imovel.setValor(body.valor());
+        imovel.setObservacao(body.observacao());
+        Imovel updatedImovel = imovelRepository.save(imovel);
+        return DtoUtils.imovelModelToDto(updatedImovel);
+    }
+
+    public void deleteById(Long id) {
+        imovelRepository.deleteById(id);
+    }
+
+    public List<ImovelDto> findByStatus(ImovelStatus status) {
+        List<Imovel> imovelList = imovelRepository.findByStatus(status);
+        return DtoUtils.convertModelList(imovelList, DtoUtils::imovelModelToDto);
+    }
+
+    public List<ImovelDto> findByTipo(ImovelTipo tipo) {
+        List<Imovel> imovelList = imovelRepository.findByTipo(tipo);
+        return DtoUtils.convertModelList(imovelList, DtoUtils::imovelModelToDto);
     }
 }
