86d0037ad0027cc98d423567e44edc2cc3e836e8
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-04-19 10:46:59 -0300

    remove inquilino repository import

--- imoveis/src/main/java/com/adm/imoveis/controller/InquilinoController.java
+++ imoveis/src/main/java/com/adm/imoveis/controller/InquilinoController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,12 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.adm.imoveis.dto.inquilino.InquilinoCreationDto;
 import com.adm.imoveis.dto.inquilino.InquilinoDto;
-import com.adm.imoveis.entities.Inquilino;
 import com.adm.imoveis.service.InquilinoService;
 
 @RequestMapping
 @RestController
 public class InquilinoController {
+
     private final InquilinoService inquilinoService;
 
     public InquilinoController(InquilinoService inquilinoService) {
@@ -36,13 +38,24 @@ public class InquilinoController {
         return ResponseEntity.status(HttpStatus.OK).body(inquilinoService.getById(id));
     }
 
-    @GetMapping(value = "inqulino")
+    @GetMapping(value = "inquilino")
     public ResponseEntity<List<InquilinoDto>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(inquilinoService.getAll());
     }
 
-    @GetMapping(value = "inquilinoByCpf")
-    public ResponseEntity<InquilinoDto> findByCpf(@RequestParam(value = "Cpf") String cpf) {
+    @GetMapping(value = "inquilino/cpf")
+    public ResponseEntity<InquilinoDto> findByCpf(@RequestParam(value = "cpf") String cpf) {
         return ResponseEntity.ok(inquilinoService.findByCpf(cpf));
+    }
+
+    @PutMapping(value = "inquilino/{id}")
+    public ResponseEntity<InquilinoDto> update(@PathVariable Long id, @RequestBody InquilinoCreationDto inquilino) {
+        return ResponseEntity.ok(inquilinoService.update(id, inquilino));
+    }
+
+    @DeleteMapping(value = "inquilino/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        inquilinoService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
