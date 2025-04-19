6ac6700ba29ff01f69e21fadcf4f64472315d018
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-04-19 12:26:35 -0300

    Adds CRUD endpoints for Repasse entity
    Introduces endpoints to create, retrieve, update, and delete Repasse records.
    
    Adds filtering options by contract ID and month/year. Enhances controller functionality to support detailed Repasse management.

--- imoveis/src/main/java/com/adm/imoveis/controller/RepasseController.java
+++ imoveis/src/main/java/com/adm/imoveis/controller/RepasseController.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adm.imoveis.dto.repasse.RepasseCreationDto;
 import com.adm.imoveis.dto.repasse.RepasseDto;
-import com.adm.imoveis.entities.Repasse;
 import com.adm.imoveis.service.RepasseService;
 
 @RequestMapping
@@ -32,5 +35,31 @@ public class RepasseController {
     @GetMapping(value = "repasse")
     public ResponseEntity<List<RepasseDto>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(repasseService.findAll());
+    }
+
+    @GetMapping(value = "repasse/{id}")
+    public ResponseEntity<RepasseDto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(repasseService.findById(id));
+    }
+
+    @GetMapping(value = "repasse/contrato/{id}")
+    public ResponseEntity<List<RepasseDto>> findByContratoId(@PathVariable Long contratoId) {
+        return ResponseEntity.ok(repasseService.findByContratoId(contratoId));
+    }
+
+    @GetMapping(value = "repasse/mes")
+    public ResponseEntity<List<RepasseDto>> findByMes(@RequestParam int mes, @RequestParam int ano) {
+        return ResponseEntity.ok(repasseService.findByMes(ano, mes));
+    }
+
+    @PutMapping(value = "repasse/{id}")
+    public ResponseEntity<RepasseDto> update(@PathVariable Long id, @RequestBody RepasseCreationDto repasse) {
+        return ResponseEntity.ok(repasseService.update(id, repasse));
+    }
+
+    @DeleteMapping(value = "repasse/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id) {
+        repasseService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
